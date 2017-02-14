533d16956ccbc8115347cde4820d4265094cb573
Joshua McMichael <josh@jmcmichael.com>
2017-02-14 17:16:49 -0600

    added favicon

--- src/app/start/start.module.ts
+++ src/app/start/start.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 import { StartComponent } from './start.component';
 import { StartRoutingModule } from './start-routing.module';
