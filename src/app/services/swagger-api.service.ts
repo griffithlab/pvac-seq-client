dd5c5d12c0d87cd5b1396fa8d1ce99e7f7c56383
Joshua McMichael <josh@jmcmichael.com>
2017-02-27 11:47:28 -0600

    whitespace

--- src/app/services/swagger-api.service.ts
+++ src/app/services/swagger-api.service.ts
@@ -6,27 +6,27 @@ import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
-export class SwaggerSchemaService {
+export class SwaggerApiService {
 
-  private url: string = 'api/dataservice/';
+    private url: string = 'api/dataservice/';
 
-  constructor(private http: Http) { }
+    constructor(private http: Http) { }
 
-  // getCustomersSummary(): Observable<ICustomer[]> {
-  //     return this.http.get(this.url + 'customers')
-  //         .map((resp: Response) => resp.json())
-  //         .catch(this.handleError);
-  // }
+    // getCustomersSummary(): Observable<ICustomer[]> {
+    //     return this.http.get(this.url + 'customers')
+    //         .map((resp: Response) => resp.json())
+    //         .catch(this.handleError);
+    // }
 
-  // updateCustomer(customer: ICustomer) {
-  //     return this.http.put(this.url + 'putCustomer/' + customer.id, customer)
-  //         .map((response: Response) => response.json())
-  //         .catch(this.handleError);
-  // }
+    // updateCustomer(customer: ICustomer) {
+    //     return this.http.put(this.url + 'putCustomer/' + customer.id, customer)
+    //         .map((response: Response) => response.json())
+    //         .catch(this.handleError);
+    // }
 
-  handleError(error: any) {
-    console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
-  }
+    handleError(error: any) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
 
 }
