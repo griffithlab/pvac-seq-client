30b7f0f59e000569f1bd615dc4b4ccbc2dbc1c8d
Joshua McMichael <josh@jmcmichael.com>
2017-02-27 11:38:40 -0600

    updated view locations in app routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,40 +5,40 @@ import { Routes, RouterModule } from '@angular/router';
 import { FullLayoutComponent } from './layouts/full-layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: FullLayoutComponent,
+    data: {
+      title: 'Home'
     },
-    {
-        path: '',
-        component: FullLayoutComponent,
-        data: {
-            title: 'Home'
-        },
-        children: [
-            {
-                path: 'dashboard',
-                loadChildren: './dashboard/dashboard.module#DashboardModule'
-            },
-            {
-                path: 'start',
-                loadChildren: './start/start.module#StartModule'
-            },
-            {
-                path: 'manage',
-                loadChildren: './manage/manage.module#ManageModule'
-            },
-            {
-                path: 'results',
-                loadChildren: './results/results.module#ResultsModule'
-            },
-        ]
-    }
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: './views/dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'start',
+        loadChildren: './views/start/start.module#StartModule'
+      },
+      {
+        path: 'manage',
+        loadChildren: './views/manage/manage.module#ManageModule'
+      },
+      {
+        path: 'results',
+        loadChildren: './views/results/results.module#ResultsModule'
+      },
+    ]
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
