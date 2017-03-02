f00fa90bbfa0f2e43464433e4bd3444d13c7ea87
Joshua McMichael <josh@jmcmichael.com>
2017-03-02 10:52:23 -0600

    process service returning all processes instead of empty object

--- src/app/services/process.service.ts
+++ src/app/services/process.service.ts
@@ -21,29 +21,36 @@ export class ProcessService {
   query() {
     return this.http
       .get(this.api + '/processes')
-      .map(this.extractData)
+      .map(mapProcesses)
       .do((data) => {
         console.log('process service data:')
         console.log(data);
       })
-      .catch(this.handleError);
+      .catch(handleError);
   }
+}
 
-  private extractData(res: Response) {
-    let body = res.json();
-    return body.data || {};
-  }
+function mapProcesses(res: Response) {
+  return res.json().map(toProcess)
+}
 
-  handleError(error: any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
+function toProcess(r: any) {
+  // TODO: create Process interface and cast responses to Processes instead of just returning the reponse.
+  // See: https://www.barbarianmeetscoding.com/blog/2016/04/02/getting-started-with-angular-2-step-by-step-6-consuming-real-data-with-http/
+  let process = r;
+  console.log('Parsed process:', process);
+  return process;
+}
+
+function handleError(error: any) {
+  let errMsg: string;
+  if (error instanceof Response) {
+    const body = error.json() || '';
+    const err = body.error || JSON.stringify(body);
+    errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+  } else {
+    errMsg = error.message ? error.message : error.toString();
   }
+  console.error(errMsg);
+  return Observable.throw(errMsg);
 }
