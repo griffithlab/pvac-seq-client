a2be00bc3d2506ee72b979406b6b009259a850d4
Joshua McMichael <josh@jmcmichael.com>
2017-03-07 11:52:27 -0600

    removed ng2-bootstrap imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { DropdownModule } from 'ng2-bootstrap/dropdown';
-import { TabsModule } from 'ng2-bootstrap/tabs';
 import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
 
-import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
 import { AsideToggleDirective } from './shared/aside.directive';
 import { BreadcrumbsComponent } from './shared/breadcrumb.component';
@@ -32,9 +29,6 @@ import { FullLayoutComponent } from './layouts/full-layout.component';
     BrowserModule,
     AppRoutingModule,
     ServicesModule,
-    DropdownModule.forRoot(),
-    TabsModule.forRoot(),
-    ChartsModule,
     InputTextModule,
     ButtonModule
   ],
