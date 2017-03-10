f7005a39185bf2078959486b22cf924cf1846f4d
Joshua McMichael <josh@jmcmichael.com>
2017-03-10 11:20:41 -0600

    moved polyfill reference to angular-cli conf from main, test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,3 @@
-import './polyfills.ts';
-
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
@@ -12,7 +10,7 @@ declare var __karma__: any;
 declare var require: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = function() { };
 
 
 Promise.all([
