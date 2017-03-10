bfea8dfd0aa0c7197a1302ab29dff1007252a852
Joshua McMichael <josh@jmcmichael.com>
2017-03-10 14:02:33 -0600

    fixed routing AOT compile error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { environment } from '../environments/environment';
 
 // Store
-import { provideStore } from '@ngrx/store';
-import { rootReducer } from './store/reducers/index.reducer';
+import { ActionReducer, combineReducers, provideStore } from '@ngrx/store';
+import { compose } from '@ngrx/core/compose';
+import { AppState } from './store/models/app.model';
+import * as fromProcesses from './store/reducers/process.reducer';
 
 // Services
 import { ConfigService } from './services/config.service';
@@ -24,6 +27,27 @@ import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
 import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
 import { AsideToggleDirective } from './shared/aside.directive';
 import { BreadcrumbsComponent } from './shared/breadcrumb.component';
+
+// AOT compilation requires a function for the root reducer
+// see: https://github.com/ngrx/store/issues/345
+const reducers = {
+  processes: fromProcesses.processes,
+  selectedProcess: fromProcesses.selectedProcess
+}
+
+// dev and prod currently provide the same function, dev adds the compose step
+// to more easily compose dev features like 'storeFreeze' that we might need in the future
+// see: https://github.com/ngrx/example-app/blob/master/src/app/reducers/index.ts#L66-L84
+const developmentReducer: ActionReducer<AppState> = compose(combineReducers)(reducers);
+const productionReducer: ActionReducer<AppState> = combineReducers(reducers);
+
+export function rootReducer(state: any, action: any) {
+  if (environment.production) {
+    return productionReducer(state, action);
+  } else {
+    return developmentReducer(state, action);
+  }
+}
 
 @NgModule({
   imports: [
