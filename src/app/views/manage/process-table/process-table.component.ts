0d888c720c1178b870baf20431eda5d1f1f62d12
Joshua McMichael <josh@jmcmichael.com>
2017-03-13 09:39:50 -0500

    installed lodash

--- src/app/views/manage/process-table/process-table.component.ts
+++ src/app/views/manage/process-table/process-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { Process } from '../../../store/models/process.model';
 
 @Component({
@@ -12,7 +12,14 @@ export class ProcessTableComponent implements OnInit {
   @Input() processes: Array<Process>;
   @Input() title: string;
 
+  @Output('reload')
+  processTableEmitter = new EventEmitter();
+
   constructor() { }
+
+  reload() {
+    this.processTableEmitter.emit('reload');
+  }
 
   ngOnInit() {
   }
