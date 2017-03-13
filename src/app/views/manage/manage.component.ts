9d1bdfc1659c39397558de72cf3192195b559f41
Joshua McMichael <josh@jmcmichael.com>
2017-03-13 13:07:40 -0500

    added types to manage component

--- src/app/views/manage/manage.component.ts
+++ src/app/views/manage/manage.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Injectable, ChangeDetectionStrategy } from '@angular/core';
 import { Router, Resolve, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
 
 import { ProcessService } from '../../services/process.service';
+import { Process } from '../../store/models/process.model';
 
 @Component({
   templateUrl: 'manage.component.html',
@@ -11,11 +13,10 @@ import { ProcessService } from '../../services/process.service';
 
 export class ManageComponent implements OnInit {
   private processes;
-  private currentProcesses$;
-  private selectedProcess$;
+  private currentProcesses$: Observable<Array<Process>>;
+  private selectedProcess$: Observable<Process>;
 
   constructor(private route: ActivatedRoute, processService: ProcessService) {
-    console.log('ManageComponent loaded.');
     this.processes = processService;
     this.currentProcesses$ = this.processes.items;
     this.processes.query();
