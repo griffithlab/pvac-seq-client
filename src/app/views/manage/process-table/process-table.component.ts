112aa3809f5d4dd3bff02cee1584714cf42c5755
Joshua McMichael <josh@jmcmichael.com>
2017-03-23 15:24:19 -0500

    got view encapsulated styles working with process-table component

--- src/app/views/manage/process-table/process-table.component.ts
+++ src/app/views/manage/process-table/process-table.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
-import { Process } from '../../../store/models/process.model';
+import { Component, ViewEncapsulation, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Process, ProcessSummaryVM } from '../../../store/models/process.model';
 
 @Component({
   selector: 'pvs-process-table',
   providers: [],
   templateUrl: './process-table.component.html',
-  styleUrls: ['./process-table.component.scss']
+  styleUrls: ['./process-table.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 
 export class ProcessTableComponent implements OnInit {
+  @Input() processSummaries: Array<ProcessSummaryVM>;
   @Input() processes: Array<Process>;
   @Input() title: string;
 
