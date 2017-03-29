de8973b2848b01c8febac70f6d86108e97d87252
Joshua McMichael <josh@jmcmichael.com>
2017-03-29 16:19:50 -0500

    create status-badge component

---
+++ src/app/views/manage/status-badge/status-badge.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'pvs-status-badge',
+  templateUrl: './status-badge.component.html',
+  styleUrls: ['./status-badge.component.scss']
+})
+export class StatusBadgeComponent {
+  @Input() running: boolean;
+  @Input() status: string;
+
+  constructor() { }
+
+}
