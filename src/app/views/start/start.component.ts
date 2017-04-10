7117fdf5d3d2aa0dbacf929df429fda46bc375b0
Joshua McMichael <josh@jmcmichael.com>
2017-04-10 10:02:35 -0500

    removed form stuff from start.component

--- src/app/views/start/start.component.ts
+++ src/app/views/start/start.component.ts
@@ -1,18 +1,4 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import * as _ from 'lodash';
-
-import { Observable } from 'rxjs/Rx';
-
-import { Store } from '@ngrx/store';
-import { AppState } from '../../store/models/app.model';
-import { File } from '../../store/models/store.model';
-import { Parameters } from '../../store/models/process.model';
-import { LoadInputsAction } from '../../store/actions/store.actions';
-
-import { InputService } from '../../services/input.service';
-
-import { SelectItem } from 'primeng/primeng';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: 'start.component.html',
@@ -21,34 +7,12 @@ import { SelectItem } from 'primeng/primeng';
 })
 
 export class StartComponent implements OnInit {
-  inputs$: Observable<SelectItem[]>;
-  parameters: {};
 
-  constructor(private store: Store<AppState>, private inputService: InputService) {
-    this.inputs$ = store
-      .select(state => state.store.inputs)
-      .map(fileMap => _.chain(fileMap)
-        .valuesIn()
-        .map((f: File) => {
-          return { label: f.display_name, value: f.fileID };
-        })
-        .value()
-      );
+  constructor() {
 
-    this.parameters = {
-      fileID: undefined,
-      samplename: ''
-    };
-
-  }
-
-  loadInputs(): void {
-    this.inputService.query()
-      .subscribe(inputs => this.store.dispatch(new LoadInputsAction(inputs)));
   }
 
   ngOnInit() {
-    this.loadInputs();
   }
 
 }
