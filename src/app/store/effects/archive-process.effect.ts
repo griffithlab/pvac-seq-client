ed7eff05743471c6321cb60830ab70382a724d90
Joshua McMichael <josh@jmcmichael.com>
2017-04-12 12:36:26 -0500

    added archive-process effect

--- src/app/store/effects/archive-process.effect.ts
+++ src/app/store/effects/archive-process.effect.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { ProcessService } from '../../services/process.service';
 import { Actions, Effect } from '@ngrx/effects';
 import { Observable } from 'rxjs/Rx';
-import { START_PROCESS_ACTION, ErrorOccurredAction } from '../actions/store.actions';
+import { ARCHIVE_PROCESS_ACTION, ErrorOccurredAction } from '../actions/store.actions';
 
 
 @Injectable()
-export class StartProcessEffectService {
+export class ArchiveProcessEffectService {
 
   constructor(private actions$: Actions, private processService: ProcessService) {
 
   }
 
   @Effect() newMessages$: Observable<any> = this.actions$
-    .ofType(START_PROCESS_ACTION)
-    .debug('sending new message to the server')
+    .ofType(ARCHIVE_PROCESS_ACTION)
+    .debug('archiving process')
     .switchMap(action => this.processService.start(action.payload))
     .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while saving message')));
 }
