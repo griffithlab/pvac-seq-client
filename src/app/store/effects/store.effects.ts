fb8ec615dbb18377bc4eec07a0528512e2fc701b
Joshua McMichael <josh@jmcmichael.com>
2017-04-21 13:47:15 -0500

    whitespace

--- src/app/store/effects/store.effects.ts
+++ src/app/store/effects/store.effects.ts
@@ -5,66 +5,66 @@ import { Observable } from 'rxjs/Rx';
 
 import { ProcessService } from '../../services/process.service';
 import {
-    LOAD_PROCESSES_ACTION,
-    LoadProcessesAction,
-    ProcessesLoadedAction,
+  LOAD_PROCESSES_ACTION,
+  LoadProcessesAction,
+  ProcessesLoadedAction,
 
-    LOAD_PROCESS_ACTION,
-    ProcessLoadedAction,
+  LOAD_PROCESS_ACTION,
+  ProcessLoadedAction,
 
-    ARCHIVE_PROCESS_ACTION,
-    ClearProcessDetailsAction,
+  ARCHIVE_PROCESS_ACTION,
+  ClearProcessDetailsAction,
 
-    ErrorOccurredAction
+  ErrorOccurredAction
 } from '../actions/store.actions';
 
 @Injectable()
 export class LoadProcessesEffectService {
-    @Effect() proceses$: Observable<Action> = this.actions$
-        .ofType(LOAD_PROCESSES_ACTION)
-        .debug('loading processes')
-        .switchMap(action => this.processService.query())
-        .map(processes => new ProcessesLoadedAction(processes))
-        .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while loading processes.')));
+  @Effect() proceses$: Observable<Action> = this.actions$
+    .ofType(LOAD_PROCESSES_ACTION)
+    .debug('loading processes')
+    .switchMap(action => this.processService.query())
+    .map(processes => new ProcessesLoadedAction(processes))
+    .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while loading processes.')));
 
-    constructor(private actions$: Actions,
-        private processService: ProcessService) { }
+  constructor(private actions$: Actions,
+    private processService: ProcessService) { }
 
 }
 
 @Injectable()
 export class LoadProcessEffectService {
-    @Effect() proces$: Observable<Action> = this.actions$
-        .ofType(LOAD_PROCESS_ACTION)
-        .debug('loading process')
-        .switchMap(action => this.processService.get(action.payload))
-        .map(process => new ProcessLoadedAction(process))
-        .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while loading process.')));
+  @Effect() proces$: Observable<Action> = this.actions$
+    .ofType(LOAD_PROCESS_ACTION)
+    .debug('loading process')
+    .switchMap(action => this.processService.get(action.payload))
+    .map(process => new ProcessLoadedAction(process))
+    .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while loading process.')));
 
-    constructor(private actions$: Actions,
-        private processService: ProcessService) { }
+  constructor(private actions$: Actions,
+    private processService: ProcessService) { }
 
 }
 
 @Injectable()
 export class ArchiveProcessEffectService {
-    @Effect() status$: Observable<Action> = this.actions$
-        .ofType(ARCHIVE_PROCESS_ACTION)
-        .debug('archiving process')
-        .switchMap((action) => {
-            return this.processService.archive(action.payload)
-                .map(() => action.payload);
-        })
-        .mergeMap((processId) => {
-            return [
-                new ClearProcessDetailsAction(processId),
-                new LoadProcessesAction()
-            ];
-        })
-        .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while archiving process.')));
+  @Effect() status$: Observable<Action> = this.actions$
+    .ofType(ARCHIVE_PROCESS_ACTION)
+    .debug('archiving process')
+    .switchMap((action) => {
+      return this.processService.archive(action.payload)
+        .map(() => action.payload);
+    })
+    .concat((processId) => {
+      return [
+        new LoadProcessesAction(),
+        new ClearProcessDetailsAction(processId),
+      ];
+    })
+    .catch(() => Observable.of(new ErrorOccurredAction('Error Ocurred while archiving process.')));
 
-    constructor(private actions$: Actions,
-        private processService: ProcessService) { }
+  constructor(private actions$: Actions,
+    private processService: ProcessService) { }
 
 }
 
