d3596a308ebd266f5e007f854c6c7f40a7c6fec1
Joshua McMichael <josh@jmcmichael.com>
2017-04-26 08:35:48 -0500

    extracting parent route params for visualization URL

--- src/app/views/results/result-visualize/result-visualize.component.ts
+++ src/app/views/results/result-visualize/result-visualize.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-result-visualize',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result-visualize.component.scss']
 })
 export class ResultVisualizeComponent implements OnInit {
-
-  constructor() { }
+  private processId;
+  private fileId;
+  private params$;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    // TODO extract route params using http://stackoverflow.com/questions/43415352/angular-2-extract-multiple-variables-from-url
+    this.params$ = this.route.params
+      .subscribe((params: Params) => {
+        this.processId = params['processId'];
+        this.fileId = params['fileId'];
+      });
   }
 
 }
