22f12f5460e8b879b6d0bf29a270b14a8edae3f5
Joshua McMichael <josh@jmcmichael.com>
2017-04-26 09:28:17 -0500

    results visualization loading properly

--- src/app/views/results/result-visualize/result-visualize.component.ts
+++ src/app/views/results/result-visualize/result-visualize.component.ts
@@ -10,23 +10,31 @@ import { DomSanitizer } from "@angular/platform-browser";
 export class ResultVisualizeComponent implements OnInit {
   private processId;
   private fileId;
-  private params$;
   private visualizeURL;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private domSanitizer: DomSanitizer,
-  ) { }
+  ) {
+    console.log('results-visualize loaded.');
+    this.processId = route.snapshot.parent.parent.params['processId'];
+    this.fileId = route.snapshot.parent.params['fileId'];
+  }
 
   ngOnInit() {
-    // TODO extract route params using http://stackoverflow.com/questions/43415352/angular-2-extract-multiple-variables-from-url
-    this.params$ = this.route.params
-      .subscribe((params: Params) => {
-        this.processId = params['processId'];
-        this.fileId = params['fileId'];
-        this.visualizeURL = this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:8080/api/v1/processes/15/results/17/visualize');
-      });
+    this.visualizeURL = this.domSanitizer
+      .bypassSecurityTrustResourceUrl(
+      'http://localhost:8080/api/v1/processes/' +
+      this.processId + '/results/' + this.fileId + '/visualize');
+
+    // // TODO extract route params using http://stackoverflow.com/questions/43415352/angular-2-extract-multiple-variables-from-url
+    // this.params$ = this.route.params
+    //   .subscribe((params: Params) => {
+    //     this.processId = params['processId'];
+    //     this.fileId = params['fileId'];
+    //     this.visualizeURL = this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:8080/api/v1/processes/15/results/17/visualize');
+    //   });
   }
 
 }
