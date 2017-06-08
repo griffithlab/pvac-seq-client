88e3e40fa748099e6037b15fb9db83ce03dac869
Joshua McMichael <josh@jmcmichael.com>
2017-06-08 12:31:46 -0500

    service.module now imports/initializes restangular

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { RestangularModule, Restangular } from 'ngx-restangular';
+
 import { ConfigService } from './config.service';
 import { SwaggerApiService } from './swagger-api.service';
 import { ProcessService } from './process.service';
 import { InputService } from './input.service';
 import { FileService } from './file.service';
 
+// Function for setting the default restangular configuration
+export function RestangularConfigFactory(RestangularProvider) {
+  const config = new ConfigService();
+  RestangularProvider.setBaseUrl(config.apiUrl());
+}
+
+
 @NgModule({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    RestangularModule.forRoot(RestangularConfigFactory),
+  ],
   providers: [
     ConfigService,
     SwaggerApiService,
