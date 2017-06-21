0626593d92d355554c5452e1aeb654d4e0f8d239
Joshua McMichael <josh@jmcmichael.com>
2017-06-21 13:11:36 -0500

    cleanup manage component controller, template

--- src/app/views/manage/manage.component.ts
+++ src/app/views/manage/manage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 import { Observable } from 'rxjs/Rx';
 import { Store } from '@ngrx/store';
