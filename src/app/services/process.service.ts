196b75d2926de829a68881fa3b4fbfe6d5127531
Joshua McMichael <josh@jmcmichael.com>
2017-07-26 16:45:16 -0500

    moved api endpoint config to environment, started defining process service calls using ngx-resource

--- src/app/services/process.service.ts
+++ src/app/services/process.service.ts
@@ -1,6 +1,8 @@
+import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
-import { URLSearchParams } from '@angular/http';
+import { URLSearchParams, RequestMethod } from '@angular/http';
 
+import { Resource, ResourceParams, ResourceAction, ResourceMethod } from 'ngx-resource';
 import { Restangular } from 'ngx-restangular';
 
 import { Observable } from 'rxjs/Rx';
@@ -13,53 +15,81 @@ import { ConfigService } from './config.service';
 
 import { FlaskQueryEncoder } from './FlaskQueryEncoder';
 
-@Injectable()
-export class ProcessService {
-  private api: string;
-
-  constructor(
-    private restangular: Restangular,
-    private config: ConfigService,
-  ) {
-    this.api = config.apiUrl();
-  }
-
-  query(): Observable<Process[]> {
-    return this.restangular
-      .all('processes')
-      .getList();
-  }
-
-  get(id: number): Observable<Process> {
-    return this.restangular
-      .one('processes', id)
-      .get();
-  }
-
-  stage(process: Process, component?: string): Observable<number> { // pvacseq-api returns new process ID
-    const body = new URLSearchParams('', new FlaskQueryEncoder());
-    const headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
-
-    if (component) { headers['X-Requesting-Component'] = component; };
-
-    const bodyArray = _.toPairs(process);
-    bodyArray.map((field) => {
-      body.append(field[0], field[1]);
-    });
-
-    return this.restangular
-      .oneUrl('staging', this.api)
-      .customPOST(
-      body,
-      'staging', // path
-      undefined, // params
-      headers
-      );
-  }
-
-  archive(id: number): Observable<string> {
-    return this.restangular
-      .one('archive', id)
-      .get();
-  }
+interface IProcessQueryParams {
+  page?: number;
+  perPage?: number;
 }
+
+interface IProcessGetParams {
+  id: number;
+}
+
+@Injectable()
+@ResourceParams({
+  url: environment.apiEndpoint,
+  pathPrefix: '/processes'
+})
+export class NewsRes extends Resource {
+
+  @ResourceAction({
+    isArray: true
+  })
+  query: ResourceMethod<IProcessGetParams, Process[]>;
+
+  @ResourceAction({
+    path: '/{!id}'
+  })
+  get: ResourceMethod<{ id: any }, Process>;
+
+}
+
+// @Injectable()
+// export class ProcessService {
+//   private api: string;
+
+//   constructor(
+//     private restangular: Restangular,
+//     private config: ConfigService,
+//   ) {
+//     this.api = config.apiUrl();
+//   }
+
+//   query(): Observable<Process[]> {
+//     return this.restangular
+//       .all('processes')
+//       .getList();
+//   }
+
+//   get(id: number): Observable<Process> {
+//     return this.restangular
+//       .one('processes', id)
+//       .get();
+//   }
+
+//   stage(process: Process, component?: string): Observable<number> { // pvacseq-api returns new process ID
+//     const body = new URLSearchParams('', new FlaskQueryEncoder());
+//     const headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+
+//     if (component) { headers['X-Requesting-Component'] = component; };
+
+//     const bodyArray = _.toPairs(process);
+//     bodyArray.map((field) => {
+//       body.append(field[0], field[1]);
+//     });
+
+//     return this.restangular
+//       .oneUrl('staging', this.api)
+//       .customPOST(
+//       body,
+//       'staging', // path
+//       undefined, // params
+//       headers
+//       );
+//   }
+
+//   archive(id: number): Observable<string> {
+//     return this.restangular
+//       .one('archive', id)
+//       .get();
+//   }
+// }
